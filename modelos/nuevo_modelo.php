bb26a4cc6512a67ae3d2e68bedfeda7c92897b43
marianamerchan2007-coder <marianamerchan2007@gmail.com>
2026-05-07 15:10:27 -0500

    corregir imagen render

--- modelos/nuevo_modelo.php
+++ modelos/nuevo_modelo.php
@@ -68,6 +68,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 include('../includes/header.php');
 include('../includes/sidebar.php');
+
+echo "<pre>";
+var_dump($_FILES['txtImagen']);
+echo "</pre>";
+exit;
 ?>
 
 <div class="main">
