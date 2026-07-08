3af7992f9bf1adef64c9992843b5289ed9bc19fd
marianamerchan2007-coder <marianamerchan2007@gmail.com>
2026-07-07 22:33:50 -0500

    btn cerrar sesion

--- calzado_web/admin/includes/sidebar.php
+++ calzado_web/admin/includes/sidebar.php
@@ -84,7 +84,7 @@ $totalProductos = $conexion->query($sqlTotal)->fetchColumn();
         <hr class="sidebar-divider">
 
     
-        <a href="../../../dashboard/index.php" class="sidebar-link" data-title="Salir">
+        <a href="../../../login/logout.php" class="sidebar-link" data-title="Salir">
             <i class="bi bi-box-arrow-left"></i>
             <span>Salir</span>
         </a>
