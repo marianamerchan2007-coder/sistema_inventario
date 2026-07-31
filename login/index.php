2890e4705f1c11bffe7a6d87317e13e4e21f9d41
marianamerchan2007-coder <marianamerchan2007@gmail.com>
2026-07-30 22:43:29 -0500

    agregar icono eye

--- login/index.php
+++ login/index.php
@@ -12,7 +12,7 @@ session_start();
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?= time(); ?>">
 </head>
 <body>
 
